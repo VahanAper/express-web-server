35e3f4df080e90b242ee84317bab2765138ffdf3
VahanAper <asryanvahandev@gmail.com>
2017-07-02 13:59:01 +0400

    render templates with hbs view engine

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome Home!',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(),
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
