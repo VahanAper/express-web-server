bb9b8ab762a5064438fe8410ead944aeeebca18c
VahanAper <asryanvahandev@gmail.com>
2017-07-02 14:24:05 +0400

    pass helper function to partials

--- server.js
+++ server.js
@@ -7,17 +7,22 @@ hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome Home!',
   });
 });
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
